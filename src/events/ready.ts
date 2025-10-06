93f5d97b1cf8e0f0d2c170d53069710c65e86c7d
spectoor72 <spectoor72@gmail.com>
2025-10-06 13:41:22 +0100

    renamed reinitialiseEvents to reinitialiseDraftEvents and corrected some ordering of the commands

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,6 +2,7 @@ import { Events } from 'discord.js';
 import { deployCommands } from '../utils/deploy-commands';
 import { config } from '../config';
 import { startReminderWorker } from '../reminders/reminderWorker';
+import { reinitialiseDraftEvents } from '../helpers/reinitialiseDraftEvents';
 
 module.exports = {
 	name: Events.ClientReady,
@@ -13,10 +14,16 @@ module.exports = {
 			for (const [guildId] of client.guilds.cache) {
 				await deployCommands({ guildId: guildId });
 				console.log(`Deployed commands to guild: ${guildId}`);
+				console.log('starting reinitialise of Draft Events in guild.');
+				await reinitialiseDraftEvents(client);
+				console.log('Reinitialised events in guild.');
 			}
 		} else {
 			await deployCommands({ guildId: config.isDev });
 			console.log(`Deployed commands to test guild: ${config.isDev}`);
+			console.log('starting reinitialise of Draft Events in Test guild.');
+			await reinitialiseDraftEvents(client);
+			console.log('Reinitialised events in test guild.');
 		}
 
 		startReminderWorker(client);
