6ce052bc0e8643da4b58625697540283e6bcd515
Lazulion <lazulionlapis@gmail.com>
2026-01-02 13:59:50 +0000

    updated misc commands to new interactions

--- src/commands/diceroll.ts
+++ src/commands/diceroll.ts
@@ -1,11 +1,12 @@
 import { SlashCommandBuilder } from "discord.js";
 import { getRandomInt } from "../helpers/generalHelpers";
+import { TrackedInteraction } from "../utils/interactionSystem";
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('diceroll')
 		.setDescription('generates a random number between 1 and 20'),
-	async execute(interaction: any) {
-		await interaction.reply('Dice roll! ' + (interaction.member.nickname ?? interaction.member.user.globalName) + ' rolled: ' + getRandomInt(1, 20) + " 🎲");
+	async execute(ix: any) {
+		await ix.reply('Dice roll! ' + (ix.interaction.member.nickname ?? ix.interaction.member.user.globalName) + ' rolled: ' + getRandomInt(1, 20) + " 🎲");
 	},
 };
